a5f2f5b10a1a8644e1fb2b365e6527c69c4f693a
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2011-07-17 20:03:56 +0200

    starting with tests

---
+++ scaldi-core/src/test/scala/org/am/scaldi/IdentifierSpec.scala
@@ -0,0 +1,44 @@
+package org.am.scaldi
+
+import org.scalatest.WordSpec
+import org.scalatest.matchers.ShouldMatchers
+import java.text.{DateFormat, SimpleDateFormat}
+
+class IdentifierSpec extends WordSpec with ShouldMatchers {
+  "Identifier" when {
+    "defaults are used" should {
+      "be converted from string" in {
+        getId("str") should be === StringIdentifier("str")
+      }
+
+      "be converted from symbol" in {
+        getId('sym) should be === StringIdentifier("sym")
+      }
+
+      "be converted from class" in {
+        getId(classOf[String]) should be === ClassIdentifier(classOf[String])
+      }
+    }
+
+    "compared" should {
+      "should not match for different identifier types" in {
+        getId(classOf[DateFormat]) sameAs getId("java.text.DateFormat") should be (false)
+      }
+
+      "should match for the same identifier types" in {
+        getId('publisher) sameAs getId("publisher") should be (true)
+        getId('user) sameAs getId("publisher") should be (false)
+
+        getId(classOf[String]) sameAs getId(classOf[String]) should be (true)
+        getId(classOf[DateFormat]) sameAs getId(classOf[String]) should be (false)
+      }
+
+      "use polymothism to compare classes" in {
+        getId(classOf[SimpleDateFormat]) sameAs getId(classOf[DateFormat]) should be (true)
+        getId(classOf[DateFormat]) sameAs getId(classOf[SimpleDateFormat]) should be (false)
+      }
+    }
+  }
+
+  def getId[T: CanBeIdentifier](target: T) = implicitly[CanBeIdentifier[T]].toIdentifier(target)
+}
