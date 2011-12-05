33c9999cd032ff7a6d9b49d0e1e7064adb19bbdb
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2011-12-05 20:03:54 +0100

    scaladoc URL improvements

---
+++ src/main/scala/scaldi/package.scala
@@ -0,0 +1,9 @@
+/**
+ * Scaldi - is Scala dependency injection library. Basically Scala already have everything you need for dependency
+ * injection. But still some things can be made easier. Goal of the project is to provide more standard and easy way
+ * to make dependency injection in Scala projects consuming power of the Scala language. With Scaldi you can define
+ * your application modules in pure Scala without any annotations or XML.
+ *
+ * For more information see: [[http://olegilyenko.github.com/scaldi/Scaldi.html]]
+ */
+package object scaldi {}
