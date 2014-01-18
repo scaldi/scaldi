ee5873d8b5bff822ae139e553be1c23cc1a06465
Oleg Ilyenko <oleg.ilyenko@gmail.com>
2014-01-18 18:21:49 +0100

    Updated to latest sbt version (0.13.1)

--- project/project/Build.scala
+++
@@ -1,9 +0,0 @@
-import sbt._
-
-object PluginDef extends Build {
-  override def projects = Seq(root)
-  lazy val root = Project("plugins", file(".")) dependsOn (ghpages, pamflet)
-
-  lazy val ghpages = uri("git://github.com/jsuereth/xsbt-ghpages-plugin.git")
-  lazy val pamflet = uri("git://github.com/n8han/pamflet-plugin#0.4.1")
-}
