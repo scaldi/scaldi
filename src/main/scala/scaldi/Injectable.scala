a930c2d59f4825cc19dcf7cee97fdb51eddd6236
Bedrytski Aliaksandr <meanor89@gmail.com>
2015-08-10 23:17:10 +0200

    Improved javadoc style (code, line breaks, no period at the end of line) Improved description of implicit parameters that I didn't know how to describe Removed unnecessary @return statements

--- src/main/scala/scaldi/Injectable.scala
+++ src/main/scala/scaldi/Injectable.scala
@@ -12,9 +12,11 @@ import scala.reflect.runtime.universe.TypeTag
  * The only responsibility of this trait is to provide you with inject function
  * (so it just provides nice syntax for injecting dependencies).
  *
+ *
  * It’s important to understand, that it’s the only purpose of it. So it is
  * completely stateless and knows nothing about actual bindings you have
  * defined in the module.
+ *
  *
  * In order to actually find and inject dependencies, "inject" method always
  * takes an implicit parameter of type Injector.
@@ -23,11 +25,12 @@ trait Injectable extends Wire {
   /**
    * Injects a dependency that was previously defined in a module
    * Example:
-   * val database = inject[Database]  // database can now be used as an instance of Database class
+   * `val database = inject[Database]  // database can now be used as an instance of Database class`
    *
-   * @param injector implicit, should be defined in the scope
-   * @param tt implicit, already defined in the scope
-   * @param nn implicit, already defined in the scope
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
+   * @param tt ensures that meta-information if type T is available at the runtime
+   * @param nn ensures, that type T is not inferred as Nothing by scala compiler,
+   *           since inject needs to know the actual type at the runtime
    * @tparam T class of the injected dependency
    * @return instance of injected dependency
    */
@@ -40,15 +43,18 @@ trait Injectable extends Wire {
    * Usually used when there are more than one injections for a defined TypeClass
    *
    * Example:
+   * {{{
    * binding identifiedBy 'host to "hostName"
    * val host = inject[String]('host)  // equals to "hostName"
    * val db = inject (by default defaultDb) // notice that TypeClass is optional in inferred in this case
+   * }}}
    *
    * @param constraints constraints helping to define what dependency of the TypeClass
    *                    to inject (typically an identifier, string or a default value)
-   * @param injector implicit, should be defined in the scope
-   * @param tt implicit, already defined in the scope
-   * @param nn implicit, already defined in the scope
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
+   * @param tt ensures that meta-information if type T is available at the runtime
+   * @param nn ensures, that type T is not inferred as Nothing by scala compiler,
+   *           since inject needs to know the actual type at the runtime
    * @tparam T class of the injected dependency
    * @return instance of injected dependency
    */
@@ -59,9 +65,11 @@ trait Injectable extends Wire {
 
   /**
    * Injects a dependency that was previously defined as a provider in a module
-   * @param injector implicit, should be defined in the scope
-   * @param tt implicit, already defined in the scope
-   * @param nn implicit, already defined in the scope
+   *
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
+   * @param tt ensures that meta-information if type T is available at the runtime
+   * @param nn ensures, that type T is not inferred as Nothing by scala compiler,
+   *           since inject needs to know the actual type at the runtime
    * @tparam T class of the injected dependency
    * @return a new instance of injected dependency
    */
@@ -74,9 +82,10 @@ trait Injectable extends Wire {
    *
    * @param constraints constraints helping to define what dependency of the TypeClass
    *                    to inject (typically an identifier, string or a default value)
-   * @param injector implicit, should be defined in the scope
-   * @param tt implicit, already defined in the scope
-   * @param nn implicit, already defined in the scope
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
+   * @param tt ensures that meta-information if type T is available at the runtime
+   * @param nn ensures, that type T is not inferred as Nothing by scala compiler,
+   *           since inject needs to know the actual type at the runtime
    * @tparam T class of the injected dependency
    * @return a new instance of injected dependency
    */
@@ -86,9 +95,11 @@ trait Injectable extends Wire {
 
   /**
    * Inject all dependencies of class T that were defined in a module
-   * @param injector implicit, should be defined in the scope
-   * @param tt implicit, already defined in the scope
-   * @param nn implicit, already defined in the scope
+   *
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
+   * @param tt ensures that meta-information if type T is available at the runtime
+   * @param nn ensures, that type T is not inferred as Nothing by scala compiler,
+   *           since inject needs to know the actual type at the runtime
    * @tparam T class of the injected dependency
    * @return a list of injections satisfying the TypeClass constraint
    */
@@ -97,10 +108,12 @@ trait Injectable extends Wire {
 
   /**
    * Inject all dependencies of class T that were defined in a module and that are defined by supplied identifiers
+   *
    * @param identifiers identifiers that were defined in module for requested injections
-   * @param injector implicit, should be defined in the scope
-   * @param tt implicit, already defined in the scope
-   * @param nn implicit, already defined in the scope
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
+   * @param tt ensures that meta-information if type T is available at the runtime
+   * @param nn ensures, that type T is not inferred as Nothing by scala compiler,
+   *           since inject needs to know the actual type at the runtime
    * @tparam T class of the injected dependency
    * @return a list of injections satisfying the TypeClass constraint and identifiers
    */
@@ -110,8 +123,9 @@ trait Injectable extends Wire {
 
   /**
    * Injects all dependencies that are defined by identifiers
+   *
    * @param identifiers identifiers that were defined in module for requested injections
-   * @param injector implicit, should be defined in the scope
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
    * @return a list of injections defined by identifiers without any TypeClass constraint
    */
   protected def injectAll(identifiers: Identifier*)(implicit injector: Injector): List[Any] =
@@ -119,24 +133,24 @@ trait Injectable extends Wire {
 
   /**
    * Setup an injection that returns default value if an injection with supplied identifiers is not found
-   * @param injector current injector
+   *
+   * @param injector current Injector, used to lookup bindings
    * @param default default value to return in case injection is not found
-   * @param ids identifiers that define requested injection
+   * @param identifiers identifiers that define requested injection
    * @tparam T class of the injected dependency
    * @return injection defined by identifiers or default value if injection is not found
    */
-  protected def injectWithDefault[T](injector: Injector, default: => T)(ids: List[Identifier]) =
-    injector getBinding ids flatMap (_.get) map (_.asInstanceOf[T]) getOrElse default
+  protected def injectWithDefault[T](injector: Injector, default: => T)(identifiers: List[Identifier]) =
+    injector getBinding identifiers flatMap (_.get) map (_.asInstanceOf[T]) getOrElse default
 
   /**
-   * ??? Injects a dependency defined by TypeClass. If dependency is not found, initializes it with constructor
+   * Injects a dependency defined by TypeClass. If dependency is not found, initializes it with constructor
    * Constructor must have a parameter paramName with a default value
-   * Uses reflection
    *
    * @param paramName name of the
-   * @param injector current injector
-   * @param tt implicit, already defined in the scope
-   * @param ct implicit, already defined in the scope
+   * @param injector implicit Injector, should be defined in the scope. It's used to lookup bindings
+   * @param tt ensures that meta-information if type T is available at the runtime
+   * @param ct ensures that meta-information if type C is available at the runtime
    * @tparam T class of the injected dependency
    * @tparam C class of constructor in case dependency cannot be found by T identifier
    * @return instance of injected dependency. If not found by, initializes one using constructor C
@@ -146,10 +160,11 @@ trait Injectable extends Wire {
 
   /**
    * Helper method for throwing a readable exception when a binding could not be found
-   * @param ids a list of identifiers
+   *
+   * @param identifiers a list of identifiers
    */
-  protected def noBindingFound(ids: List[Identifier]) =
-    throw new InjectException(ids map ("  * " +) mkString ("No binding found with following identifiers:\n", "\n", ""))
+  protected def noBindingFound(identifiers: List[Identifier]) =
+    throw new InjectException(identifiers map ("  * " +) mkString ("No binding found with following identifiers:\n", "\n", ""))
 
   /**
    * Implicit cast of classes that can be identifiers to an InjectConstraints with the identified as a first element
@@ -157,7 +172,7 @@ trait Injectable extends Wire {
    * This lets us to put the identifier in a first place to make the parameters look readable
    *
    * @param target Instance of a class that qualifies as an identifier
-   * @tparam T TypeClass of a class that qualifies as an identifier
+   * @tparam T class that qualifies as an identifier
    * @return InjectConstraints
    */
   protected implicit def canBeIdentifiedToConstraints[T : CanBeIdentifier](target: T): InjectConstraints[Nothing] =
@@ -166,20 +181,20 @@ trait Injectable extends Wire {
   /**
    * Convenience field to make the injection look more readable when used with identifiers
    * Example:
-   * inject [Database] (identified by 'local)
+   * `inject [Database] (identified by 'local)`
    */
   protected val identified = new IdentifiedWord
 
   /**
    * Convenience field to make the injection look more readable when used with default values for the injection
    * Example:
-   * inject [Database] (by default defaultDb),
+   * `inject [Database] (by default defaultDb)`
    */
   protected val by = new ByWord
 }
 
 /**
- * Exactly Injectable trait with all methods made public
+ * Exactly the same as Injectable trait, but with all methods made public
  */
 trait OpenInjectable extends Injectable {
 
@@ -204,13 +219,13 @@ trait OpenInjectable extends Injectable {
   override def injectAll(identifiers: Identifier*)(implicit injector: Injector): List[Any] =
     super.injectAll(identifiers: _*)(injector)
 
-  override def injectWithDefault[T](injector: Injector, default: => T)(ids: List[Identifier]) =
-    super.injectWithDefault[T](injector, default)(ids)
+  override def injectWithDefault[T](injector: Injector, default: => T)(identifiers: List[Identifier]) =
+    super.injectWithDefault[T](injector, default)(identifiers)
 
   override def injectWithConstructorDefault[T, C](paramName: String)(implicit injector: Injector, tt: TypeTag[T], ct: TypeTag[C]): T =
     super.injectWithConstructorDefault[T, C](paramName)(injector, tt, ct)
 
-  override def noBindingFound(ids: List[Identifier]) = super.noBindingFound(ids)
+  override def noBindingFound(identifiers: List[Identifier]) = super.noBindingFound(identifiers)
 
   override implicit def canBeIdentifiedToConstraints[T: CanBeIdentifier](target: T): InjectConstraints[Nothing] =
     super.canBeIdentifiedToConstraints[T](target)
@@ -227,9 +242,10 @@ object Injectable extends OpenInjectable
 
 /**
  * Helper class to make injection by identifier more readable
+ *
  * @param default default value for injection
  * @param initialIdentifiers identifiers
- * @tparam T TypeClass of class in default parameter
+ * @tparam T class of the default parameter
  */
 class IdentifiedWord[+T](default: Option[() => T] = None, initialIdentifiers: List[Identifier] = Nil) {
   def by[I: CanBeIdentifier](target: I*) = new InjectConstraints(default, initialIdentifiers ++ (target map implicitly[CanBeIdentifier[I]].toIdentifier))
@@ -237,6 +253,7 @@ class IdentifiedWord[+T](default: Option[() => T] = None, initialIdentifiers: Li
 
 /**
  * Helper class to make injection with default value more readable
+ *
  * @param initialIdentifiers identifiers
  */
 class ByWord(initialIdentifiers: List[Identifier] = Nil) {
@@ -246,17 +263,18 @@ class ByWord(initialIdentifiers: List[Identifier] = Nil) {
 /**
  * A wrapper for a default value for the injection and for its identifiers
  * It is here to make injection constraints look like natural language
+ *
  * @param default default value for injection
  * @param initialIdentifiers identifiers
- * @tparam T TypeClass of class in default parameter
+ * @tparam T class of the default parameter
  */
 case class InjectConstraints[+T](default: Option[() => T] = None, initialIdentifiers: List[Identifier] = Nil) {
   var identifiers : List[Identifier] = initialIdentifiers
 
   /**
    * Appends identifier to a current list of identifiers
+   *
    * @param ids one or many other identifiers
-   * @return
    */
   def and(ids: Identifier*) = {
     identifiers = identifiers ++ ids
@@ -266,50 +284,45 @@ case class InjectConstraints[+T](default: Option[() => T] = None, initialIdentif
   /**
    * Helper method to define a default value for injection
    * Example:
-   * inject [Database] ('database and by default defaultDb)
+   * `inject [Database] ('database and by default defaultDb)`
    *
    * @param by ByWord that follows the "and"
-   * @return
    */
   def and(by: ByWord) = new ByWord(identifiers)
 
   /**
    * Alias to "and"
    * Example:
-   * inject [Database] ('remote which by default defaultDb),
+   * `inject [Database] ('remote which by default defaultDb)`
    *
    * @param by ByWord that follows the "which"
-   * @return
    */
   def which(by: ByWord) = and(by)
 
   /**
    * Alias to "and"
    * Example:
-   * inject [Database] ('remote that by default defaultDb),
+   * `inject [Database] ('remote that by default defaultDb)`
    *
    * @param by ByWord that follows the "that"
-   * @return
    */
   def that(by: ByWord) = and(by)
 
   /**
    * Alias to "and"
    * Example:
-   * inject [Database] ('remote is by default defaultDb),
+   * `inject [Database] ('remote is by default defaultDb)`
    *
    * @param by ByWord that follows the "is"
-   * @return
    */
   def is(by: ByWord) = and(by)
 
   /**
    * Helper method to make possible that "identified" follows "and" word
    * Example:
-   * inject [Database] (by default defaultDb and identified by 'remote),
+   * `inject [Database] (by default defaultDb and identified by 'remote)`
    *
    * @param by IdentifiedWord that follows the "and"
-   * @return
    */
   def and(by: IdentifiedWord[_]) = new IdentifiedWord[T](default, identifiers)
 }
