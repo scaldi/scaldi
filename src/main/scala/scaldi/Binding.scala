74d4820e93a33710b25aa645c9c13f3e197e8011
Bedrytski Aliaksandr <meanor89@gmail.com>
2015-11-22 23:09:55 +0100

    Added small changes to the scaladoc

--- src/main/scala/scaldi/Binding.scala
+++ src/main/scala/scaldi/Binding.scala
@@ -3,33 +3,35 @@ package scaldi
 import scaldi.util.Util._
 
 /**
-  * Entities that extend `Identifiable` can be identified using standard Scala identifiers.
+  * Entities that extend `Identifiable` can be identified using Scaldi identifiers.
   * They can also contain conditions.
   */
 trait Identifiable {
   def identifiers: List[Identifier]
 
   /**
-   * Stores condition for binding.
-   * Used for conditional binding
-   * @return `Option` with function returning condition
-   */
+    * Stores condition for binding.
+    * Used for conditional binding
+    * @return `Option` with function returning condition
+    */
   def condition: Option[() => Condition]
 
   def isDefinedFor(desiredIdentifiers: List[Identifier]) =
     Identifier.sameAs(identifiers, desiredIdentifiers) &&
-      (condition map (_() satisfies desiredIdentifiers) getOrElse true)
+      (condition map (_ () satisfies desiredIdentifiers) getOrElse true)
 
   /**
     * Defines if binding is lazy or not
+    * False by default
     * @return `Boolean` true if not lazy, false otherwise
     */
   def isEager: Boolean = false
 
   /**
-   * Specifies if binding is cacheable
-   * @return `Boolean`
-   */
+    * Specifies if binding is cacheable
+    * False by default
+    * @return `Boolean` true if binding is cacheable, false otherwise
+    */
   def isCacheable: Boolean = false
 }
 
@@ -41,10 +43,10 @@ trait Identifiable {
   */
 trait Binding extends Identifiable {
   /**
-   * Retrieves stored binding's value, used during binding lookup during injection.
-   * If equals to `None`, binding is considered undefined
-   * @return `Option` with binding's value (or `None` if the binding is undefined)
-   */
+    * Retrieves stored binding's value, used during binding lookup during injection.
+    * If equals to `None`, binding is considered undefined
+    * @return `Option` with binding's value (or `None` if the binding is undefined)
+    */
   def get: Option[Any]
 }
 
@@ -52,16 +54,19 @@ object Binding {
   /**
     * Standard factory method for binding that from `LifecycleManager` and
     * `BindingWithLifecycle` creates new `Binding`
-    * @param lifecycleManager
-    * @param binding
+    * @param lifecycleManager `LifecycleManager` that will handle initialization / destruction events for binding
+    * @param binding `BindingWithLifecycle` actual binding
     * @return
     */
   def apply(lifecycleManager: LifecycleManager, binding: BindingWithLifecycle) = new Binding {
     def get = binding get lifecycleManager
+
     def condition = binding.condition
+
     def identifiers = binding.identifiers
 
     override def isEager = binding.isEager
+
     override def isCacheable = binding.isCacheable
   }
 }
@@ -77,7 +82,10 @@ trait BindingWithLifecycle extends Identifiable {
   def lifecycle: BindingLifecycle[Any]
 
   /**
-    * @inheritdoc
+    * Retrieves stored binding's value, used during binding lookup during injection.
+    * If equals to `None`, binding is considered undefined
+    * @param lifecycleManager `LifecycleManager` that will handle the destruction of the binding at the end of the lifecycle
+    * @return `Option` with binding's value (or `None` if the binding is undefined)
     */
   def get(lifecycleManager: LifecycleManager): Option[Any]
 }
@@ -87,29 +95,32 @@ object BindingWithLifecycle {
     val lifecycle = BindingLifecycle.empty
 
     def condition = binding.condition
+
     def identifiers = binding.identifiers
 
     def get(lifecycleManager: LifecycleManager) = binding.get
 
     override def isEager = binding.isEager
+
     override def isCacheable = binding.isCacheable
   }
 }
 
 /**
-  * Binding that is initialized on definition and never changes afterwards.
+  * Binding that is initialized at the Injector initialization phase (normally during an
+  * application startup) and never changes afterwards.
   * @param createFn function that creates and returns value that will be associated with binding
   * @param identifiers binding's identifiers (empty by default)
   * @param condition binding's conditions (empty by default)
   * @param lifecycle binding's lifecycle (empty by default)
   */
 case class NonLazyBinding(
-   private val createFn: Option[() => Any],
-   identifiers: List[Identifier] = Nil,
-   condition: Option[() => Condition] = None,
-   lifecycle: BindingLifecycle[Any] = BindingLifecycle.empty
-) extends BindingWithLifecycle {
-  lazy val target = createFn map (_() <| lifecycle.initializeObject)
+                           private val createFn: Option[() => Any],
+                           identifiers: List[Identifier] = Nil,
+                           condition: Option[() => Condition] = None,
+                           lifecycle: BindingLifecycle[Any] = BindingLifecycle.empty
+                         ) extends BindingWithLifecycle {
+  lazy val target = createFn map (_ () <| lifecycle.initializeObject)
   var destroyableAdded = false
 
   /**
@@ -147,12 +158,12 @@ case class NonLazyBinding(
   * @param lifecycle binding's lifecycle (empty by default)
   */
 case class LazyBinding(
-  private val createFn: Option[() => Any],
-  identifiers: List[Identifier] = Nil,
-  condition: Option[() => Condition] = None,
-  lifecycle: BindingLifecycle[Any] = BindingLifecycle.empty
-) extends BindingWithLifecycle {
-  lazy val target = createFn map (_() <| lifecycle.initializeObject)
+                        private val createFn: Option[() => Any],
+                        identifiers: List[Identifier] = Nil,
+                        condition: Option[() => Condition] = None,
+                        lifecycle: BindingLifecycle[Any] = BindingLifecycle.empty
+                      ) extends BindingWithLifecycle {
+  lazy val target = createFn map (_ () <| lifecycle.initializeObject)
   var destroyableAdded = false
 
   /**
@@ -185,11 +196,11 @@ case class LazyBinding(
   * @param lifecycle binding's lifecycle (empty by default)
   */
 case class ProviderBinding(
-  private val createFn: () => Any,
-  identifiers: List[Identifier] = Nil,
-  condition: Option[() => Condition] = None,
-  lifecycle: BindingLifecycle[Any] = BindingLifecycle.empty
-) extends BindingWithLifecycle {
+                            private val createFn: () => Any,
+                            identifiers: List[Identifier] = Nil,
+                            condition: Option[() => Condition] = None,
+                            lifecycle: BindingLifecycle[Any] = BindingLifecycle.empty
+                          ) extends BindingWithLifecycle {
   def target = createFn() <| lifecycle.initializeObject
 
   /**
@@ -208,22 +219,22 @@ case class ProviderBinding(
   * @param boundValue binding's value
   * @param identifiers binding's identifiers (empty by default)
   * @param condition binding's conditions (empty by default)
-  * @param cacheable
-  * @param eager
-  * @tparam T
+  * @param cacheable if binding should be cacheable (false by default)
+  * @param eager if binding is eager (non lazy) (false by default)
+  * @tparam T binding's type
   */
 case class SimpleBinding[T](
-  boundValue: Option[() => T],
-  identifiers: List[Identifier] = Nil,
-  condition: Option[() => Condition] = None,
-  cacheable: Boolean = false,
-  eager: Boolean = false
-) extends Binding {
+                             boundValue: Option[() => T],
+                             identifiers: List[Identifier] = Nil,
+                             condition: Option[() => Condition] = None,
+                             cacheable: Boolean = false,
+                             eager: Boolean = false
+                           ) extends Binding {
 
   /**
     * @inheritdoc
     */
-  lazy val get = boundValue map (_())
+  lazy val get = boundValue map (_ ())
 
   /**
     * @inheritdoc
